2ece0579dd1cdf5e70fb76e60c687413d8ffa100
kajtekkk <162376315+kajtekkk@users.noreply.github.com>
2025-06-30 09:01:48 +0200

    Delete my_code.py

--- my_code.py
+++
@@ -1,8 +0,0 @@
-                     def caluclate_sum(numbers):
-     total =0
-         for num in numbers
-      total += num
-         return total
-
-    nums = [1,2,3,4,5]
-        print(caluclate_sum(nums))
